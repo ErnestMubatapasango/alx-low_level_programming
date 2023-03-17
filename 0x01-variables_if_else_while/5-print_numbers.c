5f6f50a008933d375c318a8979ce61cc759810ec
ErnestMubatapasango <tawazzmubazz@gmail.com>
2023-03-17 04:35:23 -0700

    5-print_numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - A programm that prints a line wiith puts function
+ *
+ * Return: (0) (Success)
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 0; a < 10; a++)
+	{
+	printf("%d \n", a);
+	}
+	return (0);
+}
