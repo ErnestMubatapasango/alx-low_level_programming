5bd92bab56388ce1f3c7c7daa54c2cd79643c24b
ErnestMubatapasango <tawazzmubazz@gmail.com>
2023-03-17 04:44:03 -0700

    6-print_numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - A programm that prints a line wiith puts function
+ *
+ * Return: (0) (Success)
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 0; a < 10; a++)
+	{
+		putchar(a);
+		a++;
+	}
+	putchar('\n');
+	return (0);
+}
