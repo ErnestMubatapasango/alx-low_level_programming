8aa86652be2d7f638fcfaeb2a1937cd16f3852bc
ErnestMubatapasango <tawazzmubazz@gmail.com>
2023-04-03 04:44:15 -0700

    Task 0

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -15,12 +15,12 @@
 char *_memset(char *s, char b, unsigned int n)
 {
 
-char ptr = s;
+char *ptr = s;
 unsigned int i;
 
 for (i = 0; i < n; i++)
 {
-ptr[i] = b;
+*ptr[i] = b;
 }
-return (s);
+return (*ptr);
 }
