5ae5ee6c1dbb0e6a566d1538af0cb89861096c27
ErnestMubatapasango <tawazzmubazz@gmail.com>
2023-04-03 15:14:58 -0700

    Task  3

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -12,23 +12,23 @@
 unsigned int _strspn(char *s, char *accept)
 {
 unsigned int len = 0;
-char *p;
+unsigned char *p;
 
-for (; *s != '\0'; s++)
-{
-for (p = accept; *p != '\0'; p++)
-{
-if (*s == *p)
-{
-break;
-}
-}
-if (*p == '\0')
-{
-return (len);
-}
-len++;
-}
-return (len);
+	for (; *s != '\0'; s++)
+	{
+	for (p = accept; *p != '\0'; p++)
+	{
+	if (*s == *p)
+	{
+	break;
+	}
+	}
+	if (*p == '\0')
+	{
+	return (len);
+	}
+	len++;
+	}
+	return (len);
 }
 
