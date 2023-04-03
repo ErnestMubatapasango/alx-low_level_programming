0811e6a4bbc0898e103b194f835044f223e445f1
ErnestMubatapasango <tawazzmubazz@gmail.com>
2023-04-03 15:53:00 -0700

    Task 6

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -7,4 +7,8 @@ char *_strchr(char *s, char c);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 unsigned int _strspn(char *s, char *accept);
+void print_chessboard(char (*a)[8]);
+
+
+
 #endif
