d69c9d362233a1c4c401921fa2ba2863415deb0c
ErnestMubatapasango <tawazzmubazz@gmail.com>
2023-04-05 20:30:19 +0200

    Task 1

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+* _print_rev_recursion -> printing a string in reverse
+* @s: the string to be printed in rev
+*/
+void _print_rev_recursion(char *s)
+{
+int len, i;
+
+while (s[len] != '\0')
+{
+len++;
+}
+for (; i >= 0; i--)
+{
+_putchar(s[i]);
+_print_rev_recursion(s + 1);
+}
+_putchar('\n');
+}
+
