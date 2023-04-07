b99a028782cdad607e9af93b20224a677ed7e1cb
ErnestMubatapasango <tawazzmubazz@gmail.com>
2023-04-07 07:58:32 +0200

    Task 5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,34 +1,22 @@
+
 #include "main.h"
 
 /**
- * _sqrt_helper - checks to see if number is prime
- * @n:int
- * @min:int
- * @max:int
- * Return:int
+ * check - checks for the square root
+ * @c:int
+ * @d:int
+ *
+ * Return: int
  */
-int _sqrt_helper(int n, int min, int max)
+int check(int c, int d)
 {
-int mid = (min + max) / 2;
-if (min > max)
-{
-return (-1);
-}
-else if (mid * mid == n)
-{
-return (mid);
-}
-else if (mid * mid > n)
-{
-return (_sqrt_helper(n, min, mid - 1));
-}
-else
-{
-return (_sqrt_helper(n, mid + 1, max));
-}
+	if (c * c == d)
+		return (c);
+	if (c * c > d)
+		return (-1);
+	return (check(c + 1, d));
 }
 
-#include "main.h"
 /**
  * _sqrt_recursion - returns the natural square root of a number
  * @n: integer to find sqrt of
@@ -36,30 +24,7 @@ return (_sqrt_helper(n, mid + 1, max));
  */
 int _sqrt_recursion(int n)
 {
-if (n < 0)
-{
-return (-1);
+	if (n == 0)
+		return (0);
+	return (check(1, n));
 }
-else if (n == 0 || n == 1)
-{
-return (n);
-}
-else
-{
-int i = 1;
-while (i * i <= n)
-{
-i++;
-}
-i--;
-if (i *i == n)
-{
-return (i);
-}
-else
-{
-return (_sqrt_recursion(n - 1));
-}
-}
-}
-
