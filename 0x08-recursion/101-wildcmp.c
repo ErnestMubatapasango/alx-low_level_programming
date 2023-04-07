120dc3562fda04b3ce9f0a92dcff5ac60c32a9d3
ErnestMubatapasango <tawazzmubazz@gmail.com>
2023-04-07 07:47:57 +0200

    Task 8

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -4,20 +4,20 @@
  * str_checker - check if two strings are identical.
  * @s1: string_1 base address.
  * @s2: string_2 base address.
- * @i: left index.
- * @j: special index. (joker)
+ * @a: left index.
+ * @b: special index. (joker)
  * Return: 1 if s is palindrome, 0 otherwise.
  */
-int str_checker(char *s1, char *s2, int i, int j)
+int str_checker(char *s1, char *s2, int a, int b)
 {
 	if (s1[i] == '\0' && s2[j] == '\0')
 		return (1);
 	if (s1[i] == s2[j])
-		return (str_checker(s1, s2, i + 1, j + 1));
+		return (str_checker(s1, s2, a + 1, b + 1));
 	if (s1[i] == '\0' && s2[j] == '*')
-		return (str_checker(s1, s2, i, j + 1));
+		return (str_checker(s1, s2, a, b + 1));
 	if (s2[j] == '*')
-		return (str_checker(s1, s2, i + 1, j) || str_checker(s1, s2, i, j + 1));
+		return (str_checker(s1, s2, a + 1, b) || str_checker(s1, s2, a, b + 1));
 	return (0);
 }
 /**
