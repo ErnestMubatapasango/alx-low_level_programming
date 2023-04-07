e099a9d6d982dffb7c0bb7c0356607995a82c662
ErnestMubatapasango <tawazzmubazz@gmail.com>
2023-04-07 07:37:08 +0200

    Task 5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,4 +1,34 @@
 #include "main.h"
+
+/**
+ * _sqrt_helper - checks to see if number is prime
+ * @n:int
+ * @min:int
+ * @max:int
+ * Return:int
+ */
+int _sqrt_helper(int n, int min, int max)
+{
+int mid = (min + max) / 2;
+if (min > max)
+{
+return (-1);
+}
+else if (mid * mid == n)
+{
+return (mid);
+}
+else if (mid * mid > n)
+{
+return (_sqrt_helper(n, min, mid - 1));
+}
+else
+{
+return (_sqrt_helper(n, mid + 1, max));
+}
+}
+
+#include "main.h"
 /**
  * _sqrt_recursion - returns the natural square root of a number
  * @n: integer to find sqrt of
