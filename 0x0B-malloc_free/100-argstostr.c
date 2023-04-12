cbf6e2eda61c9f7d3ca8d5c51a278cb988b59ccb
ErnestMubatapasango <tawazzmubazz@gmail.com>
2023-04-12 02:21:56 +0200

    Task 5

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -8,40 +8,42 @@
  *
  * Return: a pointer to a new string, or NULL if it fails
  */
-
 char *argstostr(int ac, char **av)
 {
-int i, str_index = 0, arg_index = 0, total_length = 0, arg_length = 0;
-char *str;
-if (ac == 0 || av == NULL)
-{
-return (NULL);
-}
-for (i = 0; i < ac; i++)
-{
-while (av[i][arg_length] != '\0')
-{
-arg_length++;
-}
-total_length += arg_length + 1;
-}
-str = malloc(total_length *sizeof(char));
-if (str == NULL)
-{
-return (NULL);
-}
-for (i = 0; i < ac; i++)
-{
-while (av[i][arg_index] != '\0')
-{
-str[str_index] = av[i][arg_index];
-arg_index++;
-str_index++;
-}
-str[str_index] = '\n';
-str_index++;
-}
-str[str_index] = '\0';
-return (str);
-}
+	char *str, *s;
+	int i, j, k, len = 0;
 
+	if (ac == 0 || av == NULL)
+		return (NULL);
+
+	for (i = 0; i < ac; i++)
+	{
+		s = av[i];
+		j = 0;
+
+		while (s[j++])
+			len++;
+		len++;
+	}
+
+	str = (char *)malloc(sizeof(char) * (len + 1));
+	if (str == NULL)
+		return (NULL);
+
+	for (i = 0, j = 0; i < ac && j < len; i++)
+	{
+		s = av[i];
+		k = 0;
+
+		while (s[k])
+		{
+			str[j] = s[k];
+			k++;
+			j++;
+		}
+		str[j++] = '\n';
+	}
+	str[j] = '\0';
+
+	return (str);
+}
