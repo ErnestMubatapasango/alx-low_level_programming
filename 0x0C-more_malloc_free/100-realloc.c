7e51a3135a6e668253a884ff6fa3b16dd189c10d
ErnestMubatapasango <tawazzmubazz@gmail.com>
2023-04-13 14:08:02 +0200

    Task 5

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -14,6 +14,7 @@
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
 char *new_ptr;
+unsigned int i;
 
 if (new_size == 0)
 {
@@ -35,7 +36,7 @@ if (new_ptr == NULL)
 {
 return (NULL);
 }
-for (unsigned int i = 0; i < old_size; i++)
+for (i = 0; i < old_size; i++)
 {
 new_ptr[i] = ((char *) ptr)[i];
 }
