677eb8af42e83cbc647fca95efdb109da4a23aa5
ErnestMubatapasango <tawazzmubazz@gmail.com>
2023-04-19 15:30:40 +0200

    Task 3

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -11,6 +11,8 @@
 int int_index(int *array, int size, int (*cmp)(int))
 {
 int i;
+if (size > 0)
+{
 if (array != NULL && cmp != NULL)
 {
 for (i = 0; i < size; i++)
@@ -20,6 +22,7 @@ if (cmp(array[i]))
 return (i);
 }
 }
-return (-1);
 }
+return (-1);
+
 }
