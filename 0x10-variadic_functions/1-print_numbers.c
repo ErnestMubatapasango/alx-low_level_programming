d64149a361c11faadabf12d11560c41527e456fb
ErnestMubatapasango <tawazzmubazz@gmail.com>
2023-04-20 22:42:10 +0200

    Task 1

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -6,24 +6,23 @@
  * @separator: separator between two numbers
  * Return: Always 0
  */
+
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
-va_list args;
-va_start(args, n);
-unsigned int i;
+	unsigned int i;
+	va_list a;
 
-for (i = 0; i < n; i++)
-{
-int num = va_arg(args, int);
-printf("%d", num);
+	va_start(a, n);
 
-if (separator != NULL && i < n - 1)
-{
-printf("%s", separator);
+	if (separator == NULL)
+		separator = "";
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", va_arg(a, int));
+		if (i < n - 1)
+			printf("%s", separator);
+	}
+	printf("\n");
+	va_end(a);
 }
-}
-
-va_end(args);
-printf("\n");
-}
-
