4271e8ff1941ada0250b2cb38dec92adbca4dab3
ErnestMubatapasango <tawazzmubazz@gmail.com>
2023-05-04 22:29:08 +0200

    Task 3

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -10,12 +10,12 @@
  */
 int set_bit(unsigned long int *n, unsigned int index)
 {
-if (n == NULL || index >= sizeof(unsigned long int) * 8)
+if (index >= sizeof(unsigned long int) * 8)
 {
 return (-1);
 }
-unsigned long int mask = 1UL << index;
-*n |= mask;
+*n |= (1UL << index);
 return (1);
 }
+
 
